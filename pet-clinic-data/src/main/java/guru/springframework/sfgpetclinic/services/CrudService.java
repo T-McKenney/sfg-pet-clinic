685adc63c70d568790504aaea3406a4929dc487e
Tyler McKenney <tylermckenney7@gmail.com>
2023-09-30 10:35:12 -0500

    Refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -4,5 +4,17 @@ package guru.springframework.sfgpetclinic.services;
     Created by tylermckenney on 9/30/23.
 */
 
-public interface CrudService {
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findByID(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
 }
