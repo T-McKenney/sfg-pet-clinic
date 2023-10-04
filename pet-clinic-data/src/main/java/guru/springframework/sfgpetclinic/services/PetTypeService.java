9d6535a730d4cbcf1eaa33b31e734a85e2a418ca
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-04 18:36:47 -0500

    Added PetTypeService and PetTypeMapService. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+/*
+    Created by tylermckenney on 10/4/23.
+*/
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
