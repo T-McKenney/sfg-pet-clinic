e530c00b096b507bb5c0f5b8a6265730c0c09ba9
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-06 11:45:46 -0500

    Implemented Map impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+/*
+    Created by tylermckenney on 10/6/23.
+*/
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
