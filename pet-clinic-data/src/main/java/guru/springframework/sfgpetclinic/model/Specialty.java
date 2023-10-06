123d4db34758c5bee2262042ed987fa8afd2fae5
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-06 09:18:49 -0500

    Updated vet to use JPA entity mapping. Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -4,8 +4,15 @@ package guru.springframework.sfgpetclinic.model;
     Created by tylermckenney on 10/4/23.
 */
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
